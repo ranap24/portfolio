e138821e8d170b1c6cf63db102100f48088ab9ac
BalajiSai2003 <balajisai2003@outlook.com>
2025-02-03 12:27:57 +0530

    Responsive

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -12,7 +12,7 @@ export default function ContactForm() {
   };
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="my-5 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Contact Us
       </h2>
